529b7c4854be435be1950b9ce92d250de0594b45
Aminur-Rahman-Kanon <kanon754@gmail.com>
2024-07-17 00:16:19 +0100

    update client_2

--- src/Components/Pages/Topbar/Logo/logo.jsx
+++ src/Components/Pages/Topbar/Logo/logo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './logo.module.css';
-import logo from '../../../../Assets/logo.jpeg';
+import logo from '../../../../Assets/logo.png';
 
 function Logo() {
     return <img src={logo} alt="logo" className={styles.logo}/>
